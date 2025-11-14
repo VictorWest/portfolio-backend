debc6d3cec7c177ea638912bd43a7a01a404216b
VictorWest <victorwaribokowest@gmail.com>
2025-11-14 12:58:50 +0100

    finish route

---
+++ app.js
@@ -0,0 +1,46 @@
+import express from "express"
+import cors from "cors"
+import { Resend } from "resend";
+import dotenv from 'dotenv'
+dotenv.config()
+
+const app = express()
+const port = 3000
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+app.use(
+  cors({
+    origin: "http://localhost:8080",
+  })
+);
+
+app.use(express.json())
+
+app.post('/send-email', async (req, res) => {
+    const { name, email, message } = req.body
+    await sendEmail(name, email, message)
+    res.json({ success: true })
+})
+
+app.listen(port, () => console.log(`App listening on port ${port}!`))
+
+async function sendEmail(name, email, message) {
+  try {
+    const { data, error } = await resend.emails.send({
+      to: "victorwaribokowest@gmail.com",
+      from: "Victor's Portfolio Message <customerservice@merchlyach.com>",
+      subject: "You have a client message",
+      text: `
+        Name: ${name}
+        Email: ${email}
+
+        Message:
+        ${message}`,
+    });
+
+    return { data, error };
+  } catch (error) {
+    console.log("Email error:", error);
+    return { data: null, error };
+  }
+}
